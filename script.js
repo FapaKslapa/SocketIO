82e5853f0bf407c8522a4b6723d3de8182786302
FapaKslapa <maroccostefano@itis-molinari.eu>
2024-01-22 23:27:52 +0100

    Fine prima parte gitChat

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+let chats = [];
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
@@ -22,16 +22,31 @@ io.on("connection", (socket) => {
   socket.on("join room", (room) => {
     socket.join(room);
     console.log(`User joined room: ${room}`);
+    // Se la chat non esiste ancora, la creiamo
+    if (!chats.find((chat) => chat.chat === room)) {
+      chats.push({ chat: room, messaggi: [] });
+    }
   });
 
   // Ascolta i messaggi di chat e li trasmette a tutti nella stessa room
   socket.on("chat message", (room, { username, message, timestamp }) => {
     // Modifica per accettare un oggetto con username e messaggio
     io.to(room).emit("chat message", { username, message, timestamp }); // Trasmetti l'username e il messaggio
-    console.log(username + ": " + message); // Stampa l'username e il messaggio
+    let chat = chats.find((chat) => chat.chat === room);
+    if (chat) {
+      chat.messaggi.push({
+        autore: username,
+        ora: timestamp,
+        messaggio: message,
+      });
+    }
+
+    console.log(chats);
   });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+
+  socket.on("message", (data) => {});
 });
